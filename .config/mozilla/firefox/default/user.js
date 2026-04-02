4c7d017073ea9f4c150b98ef532ceab060be2f1e
Matheus <matheuslevit@gmail.com>
2026-04-02 19:07:41 -0300

    add firefox theme (Fennec)
    repo link: https://github.com/tompassarelli/fennec

---
+++ .config/mozilla/firefox/default/user.js
@@ -0,0 +1,6 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("sidebar.verticalTabs", false);
+user_pref("sidebar.revamp", false);
+user_pref("sidebar.position_start", true);
+user_pref("fennec.drawer.autohide", true);
+user_pref("fennec.urlbar.float", true);
